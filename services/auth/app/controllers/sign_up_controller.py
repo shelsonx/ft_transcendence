d665dead5bed6cba3da8aa10d8ef4cec10ca92e3
brunobonaldi94 <brunobonaldi94@gmail.com>
2024-02-25 16:38:48 -0300

    chore(base_controller): remove unnecessary comments

--- services/auth/app/controllers/sign_up_controller.py
+++ services/auth/app/controllers/sign_up_controller.py
@@ -2,11 +2,9 @@
 
 
 from ..interfaces.controllers.base_controller import BaseController
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from ..interfaces.usecase.base_usecase import BaseUseCase
 from ..dtos.sign_up_dto import SignUpDto
-from typing import Dict
-from django.core.validators import validate_email
 from ..dtos.sign_up_dto import SignUpDtoForm, SignUpDto
 
 class SignUpController(BaseController):
@@ -21,5 +19,5 @@ class SignUpController(BaseController):
   def convert_to_dto(self, data: dict) -> SignUpDto:
     return SignUpDto(email=data.get('email'), password=data.get('password'), user_name=data.get('user_name'))
 
-  async def execute(self, dto: SignUpDto) -> HttpResponse:
+  async def execute(self, dto: SignUpDto):
     return await self.sign_up_usecase.execute(dto)
--- services/auth/app/interfaces/controllers/base_controller.py
+++ services/auth/app/interfaces/controllers/base_controller.py
@@ -1,9 +1,6 @@
 from abc import ABC, abstractmethod
-import json
 from django.http import HttpRequest, HttpResponse, JsonResponse
-from typing import List, Dict
 from ...exceptions.ValidationErrorException import ValidationErrorException
-from copy import deepcopy
 from ...entities.api_data_response import ApiDataResponse
 from ...exceptions.BaseApiException import BaseApiException
 from django import forms
@@ -38,52 +35,6 @@ class BaseController(ABC):
             return self.to_json_response(data=ApiDataResponse(message=e.message, is_success=False), status=e.status_code)
         except Exception as e:
             return self.to_json_response(data=ApiDataResponse(message=str(e), is_success=False), status=500)
-
-  # def validate_parse_dto(self, request: HttpRequest) -> object:
-  #   dto = self.post_data_to_dto(request)
-  #   if self.validate(dto):
-  #     return dto
-  #   raise ValidationErrorException(message=self.errors)
-
-  # def validate(self, dto: object) -> bool:
-  #   if dto is None:
-  #     return False
-  #   for key in dto.__dict__.keys():
-  #     value = getattr(dto, key)
-  #     field = self.fields[key]
-  #     if value is None and field.is_required:
-  #       self.add_error(key, "cannot be null")
-  #     if isinstance(value, str) and value == '':
-  #       self.add_error(key, "cannot be empty")
-
-  #     for validator in field.validators:
-  #         try:
-  #           validator(value)
-  #         except Exception as e:
-  #           self.add_error(key, str(e))
-
-  #     if not isinstance(value, field.field_type):
-  #       self.add_error(key, f"must be a {field.field_type} type'")
-  #   if len(self.errors.keys()) > 0:
-  #     return False
-  #   return True
-  
-  # ## def common_validations(self, dto: object) -> bool:
-
-  # def post_data_to_dto(self, request: HttpRequest) -> object:
-  #   dto = None
-  #   current_dict = {}
-  #   for key, value in self.fields.items():
-  #     post_item = request.POST.get(value.name)
-  #     current_dict[value.name] =  post_item
-  #   self.dto = self.convert_to_dto(current_dict)
-  #   dto = deepcopy(self.dto)
-  #   return dto
-  
-  # def add_error(self, field: str, message: str) -> None:
-  #   if field not in self.errors:
-  #     self.errors[field] = []
-  #   self.errors[field].append(message)
 
   def to_json_response(self, data: ApiDataResponse, status=200) -> HttpResponse:
     return JsonResponse(status=status, data=data.to_dict(), safe=False)
