0268fe1fad4718b6bb634a33fc52853af94c5581
eliarisalvares <likah.alvares@rocketmail.com>
2024-02-27 22:04:55 -0300

    feat: user urls and setting to app

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("singlepage.urls")),
+    path("users/", include("users.urls")),
 ]
