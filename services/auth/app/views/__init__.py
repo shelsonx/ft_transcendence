c00fae02a36014c51863e3a5c44eaa853f3b6be3
brunobonaldi94 <brunobonaldi94@gmail.com>
2024-03-14 23:17:28 -0300

    feat(oauth42): oauth working fine

--- services/auth/app/views/__init__.py
+++ services/auth/app/views/__init__.py
@@ -2,4 +2,5 @@ from .sign_in_view import SignInView
 from .sign_up_view import SignUpView
 from .user_view import UserView
 from .get_user_view import GetUserView
+from .redirect_oauth42_view import RedirectOAuth42View
 from .sign_in_oauth42_view import SignInOAuth42View
