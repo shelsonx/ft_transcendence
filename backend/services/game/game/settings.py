f928a0854eba022beb9208e595de0a9462aada73
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-04-10 00:18:32 -0300

    feat: Match model

--- backend/services/game/game/settings.py
+++ backend/services/game/game/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user.apps.UserConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
