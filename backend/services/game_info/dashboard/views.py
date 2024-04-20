14ea86e3d94926c68fc7cfe826709e8f3ee284fc
shelson <shelsonx@gmail.com>
2024-04-19 23:24:28 -0300

    init implementation frontend game info in docker

--- backend/services/game_info/dashboard/views.py
+++ backend/services/game_info/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return JsonResponse({'message': 'Retornando dinamico!'})
+    #return render(request, 'index.html')
