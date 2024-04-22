02406183b8f225c36c4eb6aaee924f12635bd64f
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-04-21 21:25:44 -0300

    feat: include LANGUAGES

--- backend/services/game/game/settings.py
+++ backend/services/game/game/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+from django.utils.translation import gettext_lazy as _
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -113,6 +115,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = [
+    ("en-us", _("English")),
+    # ("pt-br", _("Portuguese")),
+    # ("es", _("Spanish")),
+]
 
 TIME_ZONE = 'UTC'
 
