e6a81949042daf1610678af2875db7d9f38cc6a3
shelson <shelsonx@gmail.com>
2024-04-24 23:29:56 -0300

    get user of the backend

--- backend/services/game_info/dashboard/admin.py
+++ backend/services/game_info/dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import UserInfo
 # Register your models here.
+
+@admin.register(UserInfo)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'nickname', 'scores', 'winnings', 'losses', 'position', 'status', 'playing', 'photo')
+    list_filter = ('full_name', 'nickname', 'scores', 'winnings', 'losses', 'position', 'status', 'playing', 'photo')
+    search_fields = ('full_name', 'nickname', 'scores', 'winnings', 'losses', 'position', 'status', 'playing', 'photo')
--- backend/services/game_info/dashboard/views.py
+++ backend/services/game_info/dashboard/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
+
+from .models import UserInfo
+
 def home(request):
-    return JsonResponse({'message': 'Retornando dinamico!'})
-    #return render(request, 'index.html')
+    users = UserInfo.objects.all()
+    data = list(users.values())
+    return JsonResponse(data, safe=False)
