50328b27104561da7481db0e342bf17d73a0259c
shelson <shelsonx@gmail.com>
2024-04-27 19:39:01 -0300

    show user details and info game..

--- backend/services/game_info/dashboard/urls.py
+++ backend/services/game_info/dashboard/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('total_infos/', views.total_infos, name='total_infos'),
+    path('user/<int:id>/', views.get_user, name='get_user'),
 ]
