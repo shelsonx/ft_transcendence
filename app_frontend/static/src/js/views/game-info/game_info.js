22575b3ce630eb6e06d72e3038c08318546e8d00
shelson <shelsonx@gmail.com>
2024-04-26 22:35:47 -0300

    refactoring dynamic data html

--- app_frontend/static/src/js/views/game-info/game_info.js
+++ app_frontend/static/src/js/views/game-info/game_info.js
@@ -86,12 +86,11 @@ const html = /*html*/`
                     </div>
                 </div>
             </div>
-        </div>
-        
+        </div>    
         <div class="containers" id="containers"></div>
     </div>
 
-` 
+`
 
 function setData(container, data) {
     const nickname = container.querySelector('.list-nickname');
@@ -115,49 +114,91 @@ function setData(container, data) {
     userPlaying.appendChild(labelPlaying);
 }
 
-function AddUserInList(data) {
-    let containers = document.getElementById('containers');
-    
+function createMainContainer() {
     const container = document.createElement('div');
     container.classList.add('container');
-    containers.appendChild(container);
+    return container;
+}
 
+function createRow(container){
     const row = document.createElement('div');
     row.classList.add('row', 'row-item-list', 'd-flex', 'align-items-center', 'mb-3');
     container.appendChild(row);
+    return row;
+}
 
+
+function createCol1(){
     const col1 = document.createElement('div');
     col1.classList.add('col-2');
-    row.appendChild(col1);
+    return col1;
+}
 
+function createUserPhoto() {
     const userPhoto = document.createElement('img');
     userPhoto.classList.add('list-user-photo');
-    col1.appendChild(userPhoto);
+    return userPhoto;
+}
 
+function createCol2() {
     const col2 = document.createElement('div');
     col2.classList.add('col-5');
-    row.appendChild(col2);
+    return col2;
+}
 
+function createNickName () {
     const nickname = document.createElement('p');
     nickname.classList.add('list-nickname', 'p-3', 'shadow');
-    col2.appendChild(nickname);
+    return nickname;
+}
 
+function createCol3() {
     const col3 = document.createElement('div');
     col3.classList.add('col-3');
-    row.appendChild(col3);
-    
+    return col3;
+}
+
+function createScores(){
     const scores = document.createElement('p');
     scores.classList.add('list-scores', 'p-3', 'shadow');
-    col3.appendChild(scores);
+    return scores;
+}
 
+function createCol4 () {
     const col4 = document.createElement('div');
     col4.classList.add('col-2');
-    row.appendChild(col4);
+    return col4;
+}
 
+function createUserGameStatus() {
     const userGameStatus = document.createElement('p');
     userGameStatus.classList.add('list-user-game-status', 'p-3', 'shadow');
-    col4.appendChild(userGameStatus);
+    return userGameStatus;
+}
 
+function AddUserInList(data) {
+    let containers = document.getElementById('containers');
+    
+    const container = createMainContainer();
+    containers.appendChild(container);
+    
+    const row = createRow(container);
+    const col1 = createCol1();
+    row.appendChild(col1);
+    const userPhoto = createUserPhoto();
+    col1.appendChild(userPhoto);
+    const col2 = createCol2();
+    row.appendChild(col2);
+    const nickname = createNickName();
+    col2.appendChild(nickname);
+    const col3 = createCol3();
+    row.appendChild(col3);
+    const scores = createScores();
+    col3.appendChild(scores);
+    const col4 = createCol4();
+    row.appendChild(col4);
+    const userGameStatus = createUserGameStatus();
+    col4.appendChild(userGameStatus);
     setData(container, data);       
 }
 
@@ -168,8 +209,6 @@ const start = () => {
           AddUserInList(data);
         });
       });
-    console.log('GameInfo View');
 }
-
 
 export default new GameInfoView(html, start);
