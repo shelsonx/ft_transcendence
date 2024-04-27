d4299084a13015cf93aee27861e9af3c1d9b1937
shelson <shelsonx@gmail.com>
2024-04-27 19:39:01 -0300

    show user details and info game..

--- backend/services/game_info/dashboard/views.py
+++ backend/services/game_info/dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 
 from .models import UserInfo
@@ -16,3 +16,11 @@ def total_infos(request):
         {'total_scores': total_scores,
         'total_players': total_players}, 
         safe=False)
+
+from django.core.serializers import serialize
+import json
+def get_user(request, id):
+    user = get_object_or_404(UserInfo, id=id)
+    serialized_data = serialize('json', [user])
+    user_dict = json.loads(serialized_data)[0]['fields']
+    return JsonResponse({'user': user_dict}, safe=False)
