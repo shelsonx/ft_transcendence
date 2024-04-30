f6e81edeabd00ee50186ac27d12199c407091720
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-04-30 01:24:07 -0300

    poc: receiving html on request

--- app_frontend/static/src/js/views/game/pong.js
+++ app_frontend/static/src/js/views/game/pong.js
@@ -1,5 +1,5 @@
 import BaseLoggedView from '../baseLoggedView.js';
-
+import gameService from '../../services/gameService.js';
 
 class PongGameView extends BaseLoggedView {
   constructor(html, start) {
@@ -13,10 +13,28 @@ class PongGameView extends BaseLoggedView {
 
 const html = /*html*/`
   <h1 class="text-bg-dark">Play pong view</h1>
+  <div id="test"></div>
+  <button class="btn btn-primary" id="button">Click me</button>
 `
 
-const start = () => {
-  console.log('Pong Game View')
+const swap = async (response) => {
+  const test = document.getElementById("test")
+  // console.log(response)
+  // const headers = await response.headers
+  // console.log(...headers)
+  // console.log(response.body)
+  test.innerHTML += await response.text()
+  // test.innerHTML = await response.text()
+}
+
+const start = async () => {
+  gameService.gameTest().then(swap);
+  console.log('Pong Game View');
+
+  const button = document.getElementById("button");
+  button.addEventListener("click", () => {
+    gameService.gameTest().then(swap);
+  })
 }
 
 export default new PongGameView(html, start);
