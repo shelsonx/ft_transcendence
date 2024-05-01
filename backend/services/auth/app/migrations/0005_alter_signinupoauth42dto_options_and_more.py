9fd5011b7d8e92403d762ddd6fc51c64e8dee736
brunobonaldi94 <brunobonaldi94@gmail.com>
2024-04-30 23:26:39 -0300

    chore: improve migrations

--- backend/services/auth/app/migrations/0005_alter_signinupoauth42dto_options_and_more.py
+++ backend/services/auth/app/migrations/0005_alter_signinupoauth42dto_options_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("app", "0005_alter_user_password"),
+        ("app", "0004_alter_user_password"),
     ]
 
     operations = [
