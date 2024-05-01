d74e05456f6901c7d3e954992ad95b98aa0d9e69
shelson <shelsonx@gmail.com>
2024-05-01 17:31:44 -0300

    set postgress 16

--- backend/services/auth/auth_api/settings.py
+++ backend/services/auth/auth_api/settings.py
@@ -39,9 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'app.apps.AppConfig',
-    "user.apps.UserConfig",
-    "core.apps.CoreConfig",
+    'app.apps.AppConfig',    
 ]
 
 MIDDLEWARE = [
