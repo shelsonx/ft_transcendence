83b88cd7da2befb932ad8e5ef6fb96bd92746519
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-05-02 00:37:21 -0300

    fix: game view

--- backend/services/game/user/models.py
+++ backend/services/game/user/models.py
@@ -1,12 +1,13 @@
-from django.db import models
 # from itertools import chain
-import uuid
 
-# Create your models here.
+# django
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    nickname = models.CharField(max_length=50)
+    id = models.UUIDField(primary_key=True, editable=False)
+    # nickname = models.CharField(max_length=50)
 
     # @property
     # def matchs(self) -> list:
