a037e1a0448360d148b55f7b1aa79bafe8e5e385
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-05-02 02:43:39 -0300

    Generator testing Game models

--- backend/services/game/test.py
+++ backend/services/game/test.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 
 from user.models import User
 from core.models import *
+from utils.generator import Generator
+
+gen = Generator()
 
 # u1 = User()
 # u1.save()
@@ -47,7 +50,7 @@ from core.models import *
 # for r in rules:
 #     pprint(vars(r))
 
-print("Can't create a duplicated rule")
+# print("Can't create a duplicated rule")
 # rule = GameRules(
 #     max_duration=None
 # )
@@ -60,29 +63,29 @@ print("Can't create a duplicated rule")
 
 # rule.delete()
 
-rule1 = GameRules(
-    rule_type=GameRuleType.MIXED_RULES,
-    points_to_win=11,
-    game_total_points=21,
-    max_duration=timedelta(minutes=5),
-)
-# rule1.full_clean()
-rule1.save()
+# rule1 = GameRules(
+#     rule_type=GameRuleType.MIXED_RULES,
+#     points_to_win=11,
+#     game_total_points=21,
+#     max_duration=timedelta(minutes=5),
+# )
+# # rule1.full_clean()
+# rule1.save()
 
-rule2 = GameRules(
-    rule_type=GameRuleType.MIXED_RULES,
-    points_to_win=11,
-    game_total_points=21,
-    max_duration=timedelta(minutes=5),
-)
-# rule2.full_clean()
-rule2.save()
+# rule2 = GameRules(
+#     rule_type=GameRuleType.MIXED_RULES,
+#     points_to_win=11,
+#     game_total_points=21,
+#     max_duration=timedelta(minutes=5),
+# )
+# # rule2.full_clean()
+# rule2.save()
 
-pprint(vars(rule1))
-pprint(vars(rule2))
+# pprint(vars(rule1))
+# pprint(vars(rule2))
 
-rule1.delete()
-rule2.delete()
+# rule1.delete()
+# rule2.delete()
 
 # rules_base = GameRules()
 # rules_base.save()
@@ -98,3 +101,72 @@ rule2.delete()
 #     player_reach_points=5,
 # )
 # rules3.save()
+
+# Generator test
+
+print()
+print(" User model test ".center(80, "-"))
+user1 = gen.seedUser()
+user2 = gen.seedUser()
+users = User.objects.all()
+print(users)
+
+print()
+print(" GameRules model test ".center(80, "-"))
+rules = GameRules.objects.all()
+print(len(rules))
+rule_default = gen.seedGameRules()
+rules = GameRules.objects.all()
+print(len(rules), rule_default)
+
+rule5 = gen.seedGameRules(
+    rule_type=GameRuleType.PLAYER_POINTS,
+    points_to_win=10,
+)
+rule6 = gen.seedGameRules(
+    points_to_win=21,
+)
+rule7 = gen.seedGameRules(
+    rule_type=GameRuleType.GAME_TOTAL_POINTS,
+    game_total_points=42,
+)
+rule8 = gen.seedGameRules(
+    rule_type=GameRuleType.GAME_DURATION,
+    max_duration=timedelta(minutes=42),
+)
+rules = GameRules.objects.all()
+print(len(rules))
+
+print()
+print(" Game model test ".center(80, "-"))
+games = Game.objects.all()
+print(len(games))
+game1 = gen.seedGame()
+pprint(vars(game1))
+
+game2 = gen.seedGame(
+    status=GameStatus.WAITING,
+    rules=rule5,
+    players=[user1, user2]
+)
+pprint(vars(game2))
+
+print()
+print(" Deleting all objects - users:")
+user1.delete()
+user2.delete()
+print(users)
+
+print(" Deleting all objects - games:")
+game1.delete()
+game2.delete()
+games = Game.objects.all()
+print(len(games))
+
+print(" Deleting all objects - rules:")
+rule5.delete()
+rule6.delete()
+rule7.delete()
+rule8.delete()
+rules = GameRules.objects.all()
+print(len(rules))
