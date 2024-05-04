a002be6978881625f7e887b6f0034344f85b022e
brunobonaldi94 <brunobonaldi94@gmail.com>
2024-05-04 20:41:56 -0300

    feat: enable translate in back - auth-api,api-gateway

--- backend/api_gateway/app/interfaces/router/router.py
+++ backend/api_gateway/app/interfaces/router/router.py
@@ -8,21 +8,23 @@ from django.http import Http404, HttpRequest
 from ...interfaces.services.http_client import IHttpClient, HttpClientData
 from ...utils.convert_to_json_response import convert_to_json_response
 from django.http import HttpResponseRedirect
+from urllib.parse import urlparse, parse_qs
+
 class Route:
-  
+
   def __init__(self, route_key, allowed_verbs, is_redirect = False) -> None:
     self.route_key = route_key
     self.allowed_verbs = allowed_verbs
     self.split = route_key.split('/')
     self.len = len(self.split)
     self.is_redirect = is_redirect
-  
+
     self.path_converters = {
-      "<str:": lambda x: str(x), 
-      "<int:": lambda x: int(x), 
+      "<str:": lambda x: str(x),
+      "<int:": lambda x: int(x),
       "<uuid:": lambda x: uuid.UUID(str(x))
     }
- 
+
   def path_converter(self, real_path: str, path: str):
     for path_convert in self.path_converters.keys():
       if path.startswith(path_convert):
@@ -38,8 +40,8 @@ class Route:
       return deepcopy(self)
     path_split = path.split('/')
     if len(path_split) != self.len:
-      return None 
-    counter = 0     
+      return None
+    counter = 0
     for i, each_path in enumerate(path_split):
       if each_path == self.split[i] or self.path_converter(each_path, self.split[i]):
         counter += 1
@@ -48,16 +50,16 @@ class Route:
     if self.len == counter:
       return deepcopy(self)
     return None
-  
+
   def __str__(self) -> str:
     return f"Route: {self.route_key} - {self.allowed_verbs}"
-  
+
 class IRouter(ABC):
 
   def __init__(self, http_client: IHttpClient, routes: List[Route]) -> None:
     self.http_client = http_client
     self.routes = { route.route_key: route for route in routes }
-  
+
 
   def path_resolver(self, path: str):
     for route in self.routes.values():
@@ -66,15 +68,34 @@ class IRouter(ABC):
         return found_route
     return None
 
+  def append_language_to_path_from_query_string(self, path):
+    url_string = self.http_client.base_url.localhost + path
+    parsed_url = urlparse(url_string)
+    query_string = parsed_url.query
+    query_params = parse_qs(query_string)
+    language = query_params.get('language')
+    if language:
+        path = path.split("?")[0]
+        query_params.pop('language')
+        query_string = "&".join([f"{key}={value[0]}" for key, value in query_params.items()])
+        path = f"{path}" + (f"?{query_string}" if query_string else "")
+        self.http_client.base_url.rebuild_url(self.http_client.base_url.path, language[0])
+        return path
+    self.http_client.base_url.rebuild_url(self.http_client.base_url.path)
+    return path
+
   def route(self, verb: str, path: str, request: HttpRequest, *args, **kwargs):
     if not isinstance(verb, str):
       raise ValueError("verb must be a string")
-
-    route = self.routes.get(path)
+    path_without_query_str = path.split("?")[0]
+    route = self.routes.get(path_without_query_str)
     if route is None:
-      route = self.path_resolver(path)
+      route = self.path_resolver(path_without_query_str)
       if route is None:
         raise Http404()
+
+    route.route_key = self.append_language_to_path_from_query_string(path)
+    print(route.route_key)
 
     if verb.upper() not in route.allowed_verbs:
       raise ValueError(f"verb '{verb}' not allowed for path '{path}'")
--- backend/services/auth/app/exceptions/field_already_exists_exception.py
+++ backend/services/auth/app/exceptions/field_already_exists_exception.py
@@ -1,6 +1,9 @@
 from .base_api_exception import BaseApiException
+from django.utils.translation import gettext_lazy as _
 
 
 class FieldAlreadyExistsException(BaseApiException):
     def __init__(self, field: str):
-        super().__init__(message=f"{field} already exists", status_code=400)
+        msg = _("field already exists")
+        print(msg)
+        super().__init__(message=f"{field} {msg}", status_code=400)
