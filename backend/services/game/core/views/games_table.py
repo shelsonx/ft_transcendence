126b0bd597e6186370f089dff63d584ae076d5dd
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-05-05 19:12:35 -0300

    WIP - games table

--- backend/services/game/core/views/games_table.py
+++ backend/services/game/core/views/games_table.py
@@ -7,10 +7,10 @@ import uuid
 from django.db.models.query import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404
-from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext_lazy as _
 from django.views import generic
 
-from core.models import Game
+from core.models import Game, GameStatus
 from user.models import User
 
 logger = logging.getLogger("eqlog")
@@ -18,9 +18,9 @@ logger = logging.getLogger("eqlog")
 
 class GamesView(generic.ListView):
     model = Game
-    ordering = "game_datetime"
-    template_name = "user_games.html"
-    paginate_by = 20
+    ordering = ["status", "game_datetime"]
+    template_name = "games_table.html"
+    # paginate_by = 20
 
     # TODO SHEELA: proteger a rota - somente o usuário pode acessar?
     def get(
@@ -39,4 +39,15 @@ class GamesView(generic.ListView):
     def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
         context["user"] = self.user
+        context["GameStatus"] = GameStatus
+
+        game_list = []
+        for game in context["game_list"]:
+            game_list.append(self.get_game_data(game))
+        context["game_list"] = game_list
+
         return context
+
+    def get_game_data(self, game: Game) -> Game:
+        game.status_label = GameStatus(game.status).label
+        return game
