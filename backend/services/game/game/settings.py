e8b229877e99f16779236faf0e1b86c536974cb2
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-05-05 19:11:41 -0300

    feat: timedelta tag

--- backend/services/game/game/settings.py
+++ backend/services/game/game/settings.py
@@ -74,6 +74,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            "libraries": {
+                "timedelta_tags": "core.templatetags.timedelta_tags",
+            },
         },
     },
 ]
