6dad8665127746f98317875ff3cd4578b5d6c320
shelson <shelsonx@gmail.com>
2024-05-12 13:57:36 -0300

    alter table UserInfo to use UUIDField and PositiveInteger

--- backend/services/game_info/dashboard/models.py
+++ backend/services/game_info/dashboard/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class UserInfo(models.Model):
+    id_msc = models.UUIDField(default=uuid.uuid4, unique=True)
     full_name = models.CharField(max_length=50, blank=False, null=False)
     nickname = models.CharField(max_length=10, blank=False, null=False)
-    scores = models.IntegerField(default=0)
-    winnings = models.IntegerField(default=0)
-    losses = models.IntegerField(default=0)
-    position = models.IntegerField()
+    scores = models.PositiveIntegerField(default=0)
+    winnings = models.PositiveIntegerField(default=0)
+    losses = models.PositiveIntegerField(default=0)
+    position = models.PositiveIntegerField(default=0)
     status = models.BooleanField(default=False)
     playing = models.BooleanField(default=False)
     photo = models.ImageField(upload_to='static/images/%Y/%m/%d/',
