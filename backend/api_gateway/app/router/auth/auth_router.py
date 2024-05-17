7339b24c43163c77a6fd09cf88ebf15a765eb255
brunobonaldi94 <brunobonaldi94@gmail.com>
2024-05-16 23:17:49 -0300

    feat: sending request to user to get user details

--- backend/api_gateway/app/router/auth/auth_router.py
+++ backend/api_gateway/app/router/auth/auth_router.py
@@ -1,13 +1,16 @@
 from abc import ABCMeta
 
 from django.http import HttpRequest
+from django.http.request import HttpHeaders
 
 from ...utils.convert_to_json_response import convert_to_json_response
+from ...utils.get_prop_from_json import get_prop_from_json
 from ...services.http_client import IHttpClient
 from ...interfaces.router.router import IRouter, Route
 from ...services.http_client import HttpClient
 from ...interfaces.services.http_client import IHttpClient, HttpClientData
 from ..api_urls import ApiUrls
+
 class AuthRouter(IRouter):
 
   def __init__(self, http_client: IHttpClient) -> None:
@@ -19,27 +22,39 @@ class AuthRouter(IRouter):
       Route("/user/", ['GET']),
       Route("/user/<uuid:user_id>", ['PUT', 'DELETE']),
       Route("/sign-in-42/", ['POST']),
-      Route("/validate-2factor-code/", ['POST', 'PUT']),
+      Route("/validate-2factor-code/", allowed_verbs=['POST', 'PUT'], handler_function=self.register),
     ]
     super().__init__(http_client, routes_auth)
 
   def register(self, http_client_data: HttpClientData, request: HttpRequest, *args, **kwargs):
-    auth_response = self.http_client.post(http_client_data)
-    auth_data = convert_to_json_response(auth_response)
-    print("auth_data", auth_data)
-    http_client = HttpClient(ApiUrls.USER_MANAGEMENT)
-    http_client_data_user_management = HttpClientData(
-      url="",
-      data={
-        "user_uuid": auth_data["id"],
-        "name": auth_data["user_name"],
-        "nickname": auth_data["user_name"],
-        "two_factor_enabled": auth_data["enable_2fa"],
-        "email": auth_data["email"],
-        "chosen_language": "en",
-        "status": "active"
-      },
-      headers=http_client_data.headers
+    if request.method == "POST":
+        return self.http_client.post(http_client_data)
+
+    sign_in_dto = self.http_client.put(http_client_data)
+    sign_in_dto_data = convert_to_json_response(sign_in_dto)
+    data = get_prop_from_json(sign_in_dto_data)
+
+    #http_client = HttpClient(ApiUrls.USER_MANAGEMENT)
+    ##########GET USER DETAILS
+    original_headers = http_client_data.headers
+    headers_dict = {k: original_headers[k] for k in original_headers}
+    headers_dict['Authorization'] = f"Bearer {data['token']}"
+
+    http_client_data_user_me = HttpClientData(
+      url="/user/",
+      data={},
+      headers=headers_dict
     )
-    http_client.post(http_client_data_user_management)
-    return auth_data
+    auth_data = self.http_client.get(http_client_data_user_me)
+    auth_data_json = convert_to_json_response(auth_data)
+    data_json = get_prop_from_json(auth_data_json)
+    print(data_json)
+    '''
+        {'id': '2f0b18d7-35b2-43c3-8091-7983356348c9', 'user_name': 'Bruno123', 'email': 'brunobonaldi94@gmail.com', 'login_type': {'name': 'email'}, 'enable_2fa': False, 'created_at': '2024-05-17T02:06:05.832Z', 'updated_at': '2024-05-17T02:06:05.832Z', 'is_active': True}
+        if enable_2fa is True:
+            nao enviar para os microservicos
+        else:
+            enviar para outros microservicos (quer dizer que o usuario esta se cadastrando)
+    '''
+    ######
+    return sign_in_dto_data
