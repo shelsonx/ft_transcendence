80697e8a8a2446e453fd5d8efa41e89ee6ba0cb7
shelson <shelsonx@gmail.com>
2024-05-21 23:57:33 -0300

    init config to multi language game-info

--- backend/services/game_info/game_info/urls.py
+++ backend/services/game_info/game_info/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('dash/', include('dashboard.urls'), name='dash'),
-]
+    path('dash/', include('dashboard.urls')),
+    prefix_default_language=False
+)
