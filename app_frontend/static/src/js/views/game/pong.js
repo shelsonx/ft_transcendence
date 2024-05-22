b985740c3d8404a4641d95c5c2da89829d32d957
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-05-22 15:26:55 -0300

    feat: pong start countdown

--- app_frontend/static/src/js/views/game/pong.js
+++ app_frontend/static/src/js/views/game/pong.js
@@ -1,44 +1,51 @@
-import BaseLoggedView from '../baseLoggedView.js';
-import gameService from '../../services/gameService.js';
-import { CANVAS_WIDTH, CANVAS_HEIGHT } from '../../constants/game.js';
-import { GameRuleType } from '../../contracts/game/gameRule.js';
-import PongManager from '../../models/pongManager.js';
-
+import BaseLoggedView from "../baseLoggedView.js";
+import gameService from "../../services/gameService.js";
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from "../../constants/game.js";
+import { GameRuleType } from "../../contracts/game/gameRule.js";
+import PongManager from "../../models/pongManager.js";
 
 class PongGameView extends BaseLoggedView {
   constructor(html, start) {
     super({
       html,
-      start
-    },
-  );
+      start,
+    });
   }
 }
 
-const html = /*html*/`
+const html = /*html*/ `
   <div id="pong-game" class="">
-    <h4 class="d-flex justify-content-center">00:00</h4>
+    <h4 class="time d-flex justify-content-center">
+      <span id="minutes">00</span>:<span id="seconds">00</span>
+    </h4>
     <div class="d-flex justify-content-sm-center">
       <div class="mx-3">
-        <span class="sm name" id="player-left-name">A</span>
-        <span class="score" id="player-left-score"> 0</span>
+        <span class="sm name" id="name-left">A</span>
+        <span class="score" id="score-left"> 0</span>
       </div>
       <div class="mx-3">
-        <span class="score" id="player-right-score">0</span>
-        <span class="sm name" id="player-right-name">B</span>
+        <span class="score" id="score-right">0</span>
+        <span class="sm name" id="name-right">B</span>
       </div>
     </div>
     <div class="d-flex justify-content-center">
       <canvas id="canvas"></canvas>
     </div>
   </div>
-`
+
+  <div id="message" class="container-fluid d-flex justify-content-center position-absolute top-50 start-50 translate-middle">
+    <button id="start" class="btn btn-primary">Start</button>
+  </div>
+`;
 
 const gameObj = {
   id: 1,
   game_datetime: new Date(),
   status: "scheduled",
-  duration: 0,
+  duration: {
+    minutes: 0,
+    seconds: 0,
+  },
   rules: {
     rule_type: GameRuleType.PLAYER_POINTS,
     points_to_win: 11,
@@ -59,6 +66,43 @@ const gameObj = {
     },
     score: 0,
   },
+};
+
+const startMessages = [
+  {
+    msg: '3',
+    showMsgDelay: 0,
+  },
+  {
+    msg: '2',
+    showMsgDelay: 1000,
+  },
+  {
+    msg: '1',
+    showMsgDelay: 2000,
+  },
+  {
+    msg: 'Get Ready!',
+    showMsgDelay: 3000,
+  },
+  {
+    msg: '',
+    showMsgDelay: 4000,
+  },
+];
+
+const loadMessages = async () => {
+  const message = document.getElementById("message");
+
+  startMessages.forEach((obj) => {
+    setTimeout(() => {
+      message.innerHTML = /*html*/ `
+        <h1>
+          ${obj.msg}
+        </h1>
+      `;
+    }, obj.showMsgDelay);
+  });
 }
 
 const start = async () => {
@@ -66,44 +110,53 @@ const start = async () => {
   const ctx = canvas.getContext("2d");
   canvas.width = CANVAS_WIDTH;
   canvas.height = CANVAS_HEIGHT;
-  let animationFrame;
 
   const pong = new PongManager(gameObj, canvas.width, canvas.height);
+  pong.table.draw(ctx);
+  pong.player_left.draw(ctx);
+  pong.player_right.draw(ctx);
 
+  let animationFrame;
   function animate() {
     if (pong.checkGameEnded() === true) {
       // save in back
       // window.cancelAnimationFrame(animationFrame);
       console.log("Ended");
-    }
-    else {
+    } else {
       pong.update();
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       pong.draw(ctx);
       animationFrame = requestAnimationFrame(animate);
     }
   }
-  animate();
 
-  window.addEventListener("keydown", (e) => {
-    switch(e.code) {
-      case "ArrowUp":
-        pong.player_right.update("up");
-        break;
-      case "ArrowDown":
-        pong.player_right.update("down");
-        break;
-      case "KeyW":
-        pong.player_left.update("up");
-        break;
-      case "KeyS":
-        pong.player_left.update("down");
-        break;
-    }
+  const start = document.getElementById("start");
+
+  start.addEventListener("click", (e) => {
+    loadMessages();
+    setTimeout(() => {
+      animate();
+    }, startMessages[4].showMsgDelay);
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.code) {
+        case "ArrowUp":
+          pong.player_right.update("up");
+          break;
+        case "ArrowDown":
+          pong.player_right.update("down");
+          break;
+        case "KeyW":
+          pong.player_left.update("up");
+          break;
+        case "KeyS":
+          pong.player_left.update("down");
+          break;
+      }
+    });
   });
 
   // window.addEventListener() // resize
-
-}
+};
 
 export default new PongGameView(html, start);
