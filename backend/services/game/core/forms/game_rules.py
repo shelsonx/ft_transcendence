1e0eed7be9be11c60c485ecbb6529a449ea44311
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-05-24 16:26:26 -0300

    add game rules and game rules options into add game flow

--- backend/services/game/core/forms/game_rules.py
+++ backend/services/game/core/forms/game_rules.py
@@ -8,9 +8,12 @@ class GameRulesForm(forms.ModelForm):
     class Meta:
         model = GameRules
         # fields = "__all__"
-        fields = ["points_to_win"]
-        widgets = {
-            "points_to_win": forms.TextInput(
-                attrs={"class": "form-control"}
-            ),
-        }
+        fields = ["rule_type", "points_to_win", "game_total_points", "max_duration"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["rule_type"].widget.attrs = {"class": "select form-select"}
+        self.fields["points_to_win"].widget.attrs = {"class": "form-control"}
+        self.fields["game_total_points"].widget.attrs = {"class": "form-control"}
+        self.fields["max_duration"].widget.attrs = {"class": "form-control"}
