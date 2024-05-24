c589a091d965a03aa9569b78f05400236ae6797d
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-05-24 12:53:11 -0300

    separate game views

--- backend/services/game/core/urls.py
+++ backend/services/game/core/urls.py
@@ -8,7 +8,7 @@ app_name = "core"
 urlpatterns = [
     path("games", views.GamesView.as_view(), name="games"),
     path("user/<uuid:pk>/games", views.GamesView.as_view(), name="user_games"),
-    path("game", views.GameView.as_view(), name="create_game"),
+    path("add-game", views.AddGameView.as_view(), name="add_game"),
     path("game/<int:pk>", views.GameView.as_view(), name="game"),
     path("tournaments", views.TournamentsView.as_view(), name="tournaments"),
     path(
