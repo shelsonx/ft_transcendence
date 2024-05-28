ce8a6eaa2e5de95a12066a3c2bde62a39e094a6f
shelson <shelsonx@gmail.com>
2024-05-27 23:32:59 -0300

    remove translate of the model

--- backend/services/game_info/dashboard/models.py
+++ backend/services/game_info/dashboard/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 import uuid
-from django.utils.translation import gettext as _
 
 # Create your models here.
 class UserInfo(models.Model):
     id_msc = models.UUIDField(default=uuid.uuid4, unique=True)
-    full_name = models.CharField(_('full_name'), max_length=50, blank=False, null=False)
-    nickname = models.CharField(_('nickname'), max_length=10, blank=False, null=False)
+    full_name = models.CharField(max_length=50, blank=False, null=False)
+    nickname = models.CharField(max_length=10, blank=False, null=False)
     scores = models.PositiveIntegerField(default=0)
     winnings = models.PositiveIntegerField(default=0)
     losses = models.PositiveIntegerField(default=0)
