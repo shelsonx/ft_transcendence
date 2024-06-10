f0d768b430e0e7e8bdac27471e6e2962a99b515e
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-06-09 21:20:12 -0300

    improve matches table

--- backend/services/game/core/views/games_list.py
+++ backend/services/game/core/views/games_list.py
@@ -33,14 +33,23 @@ class GamesView(generic.ListView):
         # pprint.pprint(request.headers, indent=4)
         self.user = None
         if pk:
-            self.user = pk
+            self.user = User.objects.filter(pk=pk).first()  # TODO: usar a regra abaixo
+            if not self.user:
+                self.user = get_object_or_404(User, username="sheela")
             # self.user = get_object_or_404(User, pk=pk)
         return super().get(request, *args, **kwargs)
 
     def get_queryset(self) -> QuerySet[Game]:
-        # if self.user:
-        #     return self.user.games.all()
-        return super().get_queryset()
+        if self.user:
+            return self.user.games.all()
+
+        excluded_status = [
+            GameStatus.PENDING,
+            GameStatus.SCHEDULED,
+            GameStatus.CANCELED,
+        ]
+        queryset = super().get_queryset()
+        return queryset.exclude(status__in=excluded_status)
 
     def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
@@ -56,4 +65,16 @@ class GamesView(generic.ListView):
 
     def get_game_data(self, game: Game) -> Game:
         game.status_label = GameStatus(game.status).label
+
+        player_left, player_right = game.players
+        player_left.is_winner = False
+        player_right.is_winner = False
+        winner = game.winner
+        if winner and winner.pk == player_left.user.pk:
+            player_left.is_winner = True
+        else:
+            player_right.is_winner = True
+        game.player_left = player_left
+        game.player_right = player_right
+
         return game
