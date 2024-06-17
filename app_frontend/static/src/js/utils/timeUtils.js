42bf6213a3dbd155857a38f11d8e9cc7d1c02e95
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-06-17 12:06:07 -0300

    feat: PAUSE game in front

--- app_frontend/static/src/js/utils/timeUtils.js
+++ app_frontend/static/src/js/utils/timeUtils.js
@@ -1,7 +1,19 @@
 export const getTimeValue = (value) => {
-  if (!value)
-    return "00";
-  if (value < 10)
-    return `0${value}`;
+  if (!value) return "00";
+  if (value < 10) return `0${value}`;
   return `${value}`;
-}
+};
+
+export const timeDeltaToDuration = (delta) => {
+  const minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((delta % (1000 * 60)) / 1000);
+
+  return {
+    minutes: minutes,
+    seconds: seconds,
+  };
+};
+
+export const durationToTimeDelta = (duration) => {
+  return (duration.seconds + duration.minutes * 60) * 1000;
+};
