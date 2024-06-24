d2bcfde980ae926edc71546e710fdd73027272ce
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-06-24 00:21:57 -0300

    fix: hashChangeHandler

--- app_frontend/static/src/js/utils/hashChangeHandler.js
+++ app_frontend/static/src/js/utils/hashChangeHandler.js
@@ -1,3 +1,13 @@
 export const hashChangeHandler = (e) => {
-  window.location.href = window.location.origin + window.location.hash;
+  const notApplicableHashes = [
+    "#view-user-games",
+    "#pong",
+    "#verify-player",
+    "#tournament",
+    "#verify-players",
+    "#two-factor-auth"
+  ]
+  const hash = window.location.hash;
+  if (!notApplicableHashes.includes(hash))
+    window.location.href = window.location.origin + window.location.hash;
 };
