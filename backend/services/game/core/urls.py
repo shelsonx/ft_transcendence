95922626cf60cd04bda443b3991d13ad4eae4029
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-06-24 01:55:25 -0300

    feat: add translation in game service - wip

--- backend/services/game/core/urls.py
+++ backend/services/game/core/urls.py
@@ -8,15 +8,23 @@ app_name = "core"
 urlpatterns = [
     path("games", views.GamesView.as_view(), name="games"),
     path("user-games/<uuid:pk>", views.GamesView.as_view(), name="user_games"),
-    path("view-user-games/<uuid:pk>", views.GamesView.as_view(), name="view_user_games"),
-
+    path(
+        "view-user-games/<uuid:pk>", views.GamesView.as_view(), name="view_user_games"
+    ),
     path("game", views.AddGameView.as_view(), name="add_game"),
-    path("game-validation/<int:pk>", views.ValidateGameView.as_view(), name="validate_game"),
+    path(
+        "game-validation/<int:pk>",
+        views.ValidateGameView.as_view(),
+        name="validate_game",
+    ),
     path("game/<int:pk>", views.GameView.as_view(), name="game"),
-
     path("tournaments", views.TournamentsView.as_view(), name="tournaments"),
     path("tournaments/<uuid:pk>", views.TournamentsView.as_view(), name="tournaments"),
     path("tournament", views.AddTournamentView.as_view(), name="add_tournament"),
     path("tournament/<int:pk>", views.TournamentView.as_view(), name="tournament"),
-    path("tournament-validation/<int:pk>", views.ValidateTournamentView.as_view(), name="validate_tournament"),
+    path(
+        "tournament-validation/<int:pk>",
+        views.ValidateTournamentView.as_view(),
+        name="validate_tournament",
+    ),
 ]
