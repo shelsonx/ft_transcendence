4e301dbfaef4a7e80c7dd2ee2034f09e34e231ed
Sheela Khusal <sheela.khusal.08@gmail.com>
2024-06-24 22:34:54 -0300

    fix: loaddata order

--- backend/services/game/entrypoint.sh
+++ backend/services/game/entrypoint.sh
@@ -7,8 +7,8 @@ python manage.py migrate
 python manage.py loaddata game_rules
 python manage.py loaddata game
 python manage.py loaddata game_player
-python manage.py loaddata round
 python manage.py loaddata tournament
+python manage.py loaddata round
 python manage.py loaddata tournament_player
 
 exec $@
